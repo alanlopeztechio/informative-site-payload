d9d2ee0b7cfd4fae1efc76b3356f900f190591d7
alanlopeztechio <alan.lopez@g-tech.io>
2025-07-16 14:44:49 -0600

    Creating migrations db

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250629_202644_initial from './20250629_202644_initial'
+import * as migration_20250629_202644_initial from './20250629_202644_initial';
+import * as migration_20250716_204415 from './20250716_204415';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250629_202644_initial.down,
     name: '20250629_202644_initial',
   },
-]
+  {
+    up: migration_20250716_204415.up,
+    down: migration_20250716_204415.down,
+    name: '20250716_204415'
+  },
+];
