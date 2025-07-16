1b359a02b5efd11a7cadf6cc9dcef547295ca8c1
alanlopeztechio <alan.lopez@g-tech.io>
2025-07-16 16:58:29 -0600

    Refactor code structure for improved readability and maintainability

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20250716_214745 from './20250716_214745';
-import * as migration_20250716_214917 from './20250716_214917';
+import * as migration_20250716_225356 from './20250716_225356';
 
 export const migrations = [
   {
-    up: migration_20250716_214745.up,
-    down: migration_20250716_214745.down,
-    name: '20250716_214745',
-  },
-  {
-    up: migration_20250716_214917.up,
-    down: migration_20250716_214917.down,
-    name: '20250716_214917'
+    up: migration_20250716_225356.up,
+    down: migration_20250716_225356.down,
+    name: '20250716_225356'
   },
 ];
