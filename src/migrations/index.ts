b64507535c869779eb26c3b0789224360f52eca1
alanlopeztechio <alan.lopez@g-tech.io>
2025-07-17 13:01:12 -0600

    feat(migration): add carousel block tables and relationships
    - Created tables for carousel images and blocks with necessary fields.
    - Established foreign key constraints for data integrity.
    - Added indexes for improved query performance.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250629_202644_initial from './20250629_202644_initial';
 import * as migration_20250716_204415 from './20250716_204415';
+import * as migration_20250717_190000 from './20250717_190000';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250716_204415.up,
     down: migration_20250716_204415.down,
-    name: '20250716_204415'
+    name: '20250716_204415',
+  },
+  {
+    up: migration_20250717_190000.up,
+    down: migration_20250717_190000.down,
+    name: '20250717_190000'
   },
 ];
