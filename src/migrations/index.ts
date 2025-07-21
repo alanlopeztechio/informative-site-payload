247031d14b2fc96c816b6d46ad948d3cd737eaee
SaraCaloso <sarahi.caloso@g-tech.io>
2025-07-21 10:10:49 -0600

    ahi te va x2

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,21 +1,9 @@
-import * as migration_20250716_225356 from './20250716_225356';
-import * as migration_20250721_155651 from './20250721_155651';
-import * as migration_20250721_160730 from './20250721_160730';
+import * as migration_20250721_161038 from './20250721_161038';
 
 export const migrations = [
   {
-    up: migration_20250716_225356.up,
-    down: migration_20250716_225356.down,
-    name: '20250716_225356',
-  },
-  {
-    up: migration_20250721_155651.up,
-    down: migration_20250721_155651.down,
-    name: '20250721_155651',
-  },
-  {
-    up: migration_20250721_160730.up,
-    down: migration_20250721_160730.down,
-    name: '20250721_160730'
+    up: migration_20250721_161038.up,
+    down: migration_20250721_161038.down,
+    name: '20250721_161038'
   },
 ];
