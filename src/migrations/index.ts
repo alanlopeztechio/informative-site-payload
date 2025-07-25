d563efde8453da7c181690fff87868c2f644c91a
alanlopeztechio <alan.lopez@g-tech.io>
2025-07-25 13:18:35 -0600

    feat(migrations): add initial migration for pages blocks structure
    - Create ENUM types for column widths
    - Define tables for text, column, row, and section blocks
    - Establish foreign key relationships with pages and _pages_v tables
    - Add indexes for improved query performance

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250721_184054 from './20250721_184054';
 import * as migration_20250721_190139 from './20250721_190139';
+import * as migration_20250725_191753 from './20250725_191753';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250721_190139.up,
     down: migration_20250721_190139.down,
-    name: '20250721_190139'
+    name: '20250721_190139',
+  },
+  {
+    up: migration_20250725_191753.up,
+    down: migration_20250725_191753.down,
+    name: '20250725_191753'
   },
 ];
