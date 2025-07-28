bd2577a7fa0424fdc3733adb4490fd7f09599d24
SaraCaloso <sarahi.caloso@g-tech.io>
2025-07-28 13:57:44 -0600

    cambios

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -16,7 +16,7 @@ import { Header } from './Header/config'
 import { plugins } from './plugins'
 import { defaultLexical } from '@/fields/defaultLexical'
 import { getServerSideURL } from './utilities/getURL'
-
+import SiteConfig from './globals/siteConfig'
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
@@ -66,7 +66,7 @@ export default buildConfig({
   }),
   collections: [Pages, Posts, Media, Categories, Users],
   cors: [getServerSideURL()].filter(Boolean),
-  globals: [Header, Footer],
+  globals: [Header, Footer, SiteConfig],
   plugins: [
     ...plugins,
     vercelBlobStorage({
