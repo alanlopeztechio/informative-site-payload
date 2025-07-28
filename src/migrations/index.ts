40369aec3b4a28ad5340a66f0ecf0587f83a07b5
alanlopeztechio <alan.lopez@g-tech.io>
2025-07-28 11:13:34 -0600

    feat(migration): add initial migration file for database schema changes

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20250725_215914 from './20250725_215914';
 import * as migration_20250728_154855 from './20250728_154855';
 import * as migration_20250728_155506 from './20250728_155506';
 import * as migration_20250728_165821 from './20250728_165821';
+import * as migration_20250728_171010 from './20250728_171010';
 
 export const migrations = [
   {
@@ -22,6 +23,11 @@ export const migrations = [
   {
     up: migration_20250728_165821.up,
     down: migration_20250728_165821.down,
-    name: '20250728_165821'
+    name: '20250728_165821',
+  },
+  {
+    up: migration_20250728_171010.up,
+    down: migration_20250728_171010.down,
+    name: '20250728_171010'
   },
 ];
