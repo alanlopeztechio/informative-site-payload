fea4fb4594d2eaec76da13b5657660b1924edf86
alanlopeztechio <alan.lopez@g-tech.io>
2025-07-30 15:42:15 -0600

    Change my life, I think that It

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import * as migration_20250730_165417 from './20250730_165417'
 
 export const migrations = [
@@ -7,3 +8,14 @@ export const migrations = [
     name: '20250730_165417',
   },
 ]
+=======
+import * as migration_20250730_214119 from './20250730_214119';
+
+export const migrations = [
+  {
+    up: migration_20250730_214119.up,
+    down: migration_20250730_214119.down,
+    name: '20250730_214119'
+  },
+];
+>>>>>>> dd3b83b1 (Change my life, I think that It)
